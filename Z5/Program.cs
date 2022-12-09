b2df876401a8e4df0776fcc7562e5ffded7bbb27
Ekaterina <kmatway@yandex.ru>
2022-12-09 22:35:52 +0500

    Z5

---
+++ Z5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username == "катя")
+{
+      Console.WriteLine("Ура, Катя!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
