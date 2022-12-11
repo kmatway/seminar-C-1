1bdbc15f2563cd6d03148f74a14c9230c6303cc0
Ekaterina <kmatway@yandex.ru>
2022-12-11 13:24:05 +0500

    Z10

---
+++ Z10/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите номер дня недели: = ");
+int number = int.Parse(Console.ReadLine());
+
+if(number == 3)
+{
+    Console.WriteLine("Среда");
+}
+if(number == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if(number == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if(number == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if(number == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if(number == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if(number == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+if(number > 7)
+{
+    Console.WriteLine("Дней недели всего семь");
+}
