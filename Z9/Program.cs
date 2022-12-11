dda93df3503976581e31fcfbd7bc9f70db2341c1
Ekaterina <kmatway@yandex.ru>
2022-12-11 13:09:58 +0500

    Z9

---
+++ Z9/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число a = ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число b = ");
+int b = int.Parse(Console.ReadLine());
+
+if(a == b*b)
+{
+  Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+
